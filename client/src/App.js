1b3820e7fcf89aa6ad1ae8584e20dee556d8818b
codershani <codershani13@gmail.com>
2024-07-25 23:37:52 +0530

    Edit URL

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("http://localhost:8000")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
