94db29e1a6323e169645e9f478dae59c0378ec90
Shani Sharma <141328315+shanisharrma@users.noreply.github.com>
2024-07-25 23:49:28 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 // middleware
 const corsOptions = {
-  origin: "https://invest-app-frontend.onrender.com/", // frontend URI (ReactJS)
+  origin: "https://invest-app-frontend.onrender.com", // frontend URI (ReactJS)
 };
 app.use(express.json());
 app.use(cors(corsOptions));
